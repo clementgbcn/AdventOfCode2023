2baa847ee83b87aa4c4ec8912c3b9a2e24282fa4
Clement Gaboriau Couanau <clement.gaboriaucouanau@datadoghq.com>
2023-12-15 00:13:08 +0100

    [Day14] Optimise solution

--- days/day_14.py
+++ days/day_14.py
@@ -1,130 +1,150 @@
+import bisect
 from dataclasses import dataclass
 from enum import Enum
-from functools import cmp_to_key
 from typing import Iterator
 
 from day_factory.day import Day
 
 
 class Direction(Enum):
-    NORTH = "north", [-1, 1, -1, 1]
-    SOUTH = "south", [1, -1, -1, 1]
-    WEST = "east", [-1, 1, -1, 1]
-    EAST = "west", [-1, 1, 1, -1]
-
-    @property
-    def ordering(self):
-        return self.value[1]
+    NORTH = "north"
+    SOUTH = "south"
+    WEST = "east"
+    EAST = "west"
 
 
 @dataclass
 class Grid:
-    rounded: list[complex]
-    rounded_set: set[complex]
-    cubes: set[complex]
+    rounded_by_row: dict[int, list[int]]
+    rounded_by_col: dict[int, list[int]]
+    ordered_row: list[int]
+    ordered_col: list[int]
+    cubes: set[tuple[int, int]]
     nb_row: int
     nb_col: int
 
     @classmethod
     def build_from_string(cls, grid_str: Iterator[str]):
         cubes = set()
-        rounded = []
-        i = complex(0, 1)
+        rounded_by_col = {}
+        ordered_col = []
         nb_row, nb_col = 0, 0
         for row, line in enumerate(grid_str):
             nb_col = len(line)
             for col, char in enumerate(line):
                 if char == "#":
-                    cubes.add(row * i + col)
+                    cubes.add((row, col))
                 elif char == "O":
-                    rounded.append(row * i + col)
+                    rounded_by_col[col] = rounded_by_col.get(col, []) + [row]
+                    if len(rounded_by_col[col]) == 1:
+                        bisect.insort(ordered_col, col)
             nb_row = row
         nb_row += 1
-        return cls(rounded, set(rounded), cubes, nb_row, nb_col)
+        return cls({}, rounded_by_col, [], ordered_col, cubes, nb_row, nb_col)
 
     def get_hash(self):
         hsh = ""
-        for x in range(self.nb_row):
-            for y in range(self.nb_col):
-                hsh += "#" if x * complex(0, 1) + y in self.rounded_set else "."
+        for j, rows in self.rounded_by_col.items():
+            col = ["."] * self.nb_row
+            for i in rows:
+                col[i] = "#"
+            hsh += "".join(col)
         return hsh
 
-    @staticmethod
-    def get_key_cmp(direction: Direction):
-        def key_cmp(a, b):
-            return Grid.sort_direction(a, b, direction)
+    def get_sum(self):
+        total = 0
+        for j, rows in self.rounded_by_col.items():
+            for i in rows:
+                total += self.nb_row - i
+        return total
 
-        return cmp_to_key(key_cmp)
-
-    @staticmethod
-    def sort_direction(a: complex, b: complex, direction: Direction):
-        if a.imag < b.imag:
-            return direction.ordering[0]
-        elif a.imag > b.imag:
-            return direction.ordering[1]
-        elif a.real < b.real:
-            return direction.ordering[2]
-        elif a.real > b.real:
-            return direction.ordering[3]
-        return 0
+    def get_sum_from_row(self):
+        total = 0
+        for i, cols in self.rounded_by_row.items():
+            total += (self.nb_row - i) * len(cols)
+        return total
 
     def process_direction(self, direction: Direction):
-        self.rounded = sorted(self.rounded, key=Grid.get_key_cmp(direction))
-        new_rounded_set = set()
         if direction == Direction.NORTH:
-            self.process_north(new_rounded_set)
+            self.process_north()
         elif direction == Direction.SOUTH:
-            self.process_south(new_rounded_set)
+            self.process_south()
         elif direction == Direction.EAST:
-            self.process_east(new_rounded_set)
+            self.process_east()
         else:
-            self.process_west(new_rounded_set)
-        self.rounded_set = new_rounded_set
-        self.rounded = list(new_rounded_set)
+            self.process_west()
 
-    def process_north(self, new_rounded_set):
-        for rock in self.rounded:
-            res = rock - complex(0, 1)
-            while (
-                res.imag >= 0 and res not in self.cubes and res not in new_rounded_set
-            ):
-                res -= complex(0, 1)
-            res += complex(0, 1)
-            new_rounded_set.add(res)
+    def process_north(self):
+        self.rounded_by_row = {}
+        self.ordered_row = []
+        for j in sorted(self.rounded_by_col.keys()):
+            for i in self.rounded_by_col[j]:
+                k = i - 1
+                while (
+                    k >= 0
+                    and (k, j) not in self.cubes
+                    and j > self.rounded_by_row.get(k, [-1])[-1]
+                ):
+                    k -= 1
+                k += 1
+                if k not in self.rounded_by_row:
+                    self.rounded_by_row[k] = []
+                    bisect.insort(self.ordered_row, k)
+                self.rounded_by_row[k].append(j)
 
-    def process_south(self, new_rounded_set):
-        for rock in self.rounded:
-            res = rock + complex(0, 1)
-            while (
-                res.imag < self.nb_row
-                and res not in self.cubes
-                and res not in new_rounded_set
-            ):
-                res += complex(0, 1)
-            res -= complex(0, 1)
-            new_rounded_set.add(res)
+    def process_south(self):
+        self.rounded_by_row = {}
+        self.ordered_row = []
+        for j in self.ordered_col:
+            for i in self.rounded_by_col[j][::-1]:
+                k = i + 1
+                while (
+                    k < self.nb_row
+                    and (k, j) not in self.cubes
+                    and j > self.rounded_by_row.get(k, [-1])[-1]
+                ):
+                    k += 1
+                k -= 1
+                if k not in self.rounded_by_row:
+                    self.rounded_by_row[k] = []
+                    bisect.insort(self.ordered_row, k)
+                self.rounded_by_row[k].append(j)
 
-    def process_east(self, new_rounded_set):
-        for rock in self.rounded:
-            res = rock + 1
-            while (
-                res.real < self.nb_col
-                and res not in self.cubes
-                and res not in new_rounded_set
-            ):
-                res += 1
-            res -= 1
-            new_rounded_set.add(res)
+    def process_east(self):
+        self.rounded_by_col = {}
+        self.ordered_col = []
+        for i in self.ordered_row:
+            for j in self.rounded_by_row[i][::-1]:
+                k = j + 1
+                while (
+                    k < self.nb_col
+                    and (i, k) not in self.cubes
+                    and i > self.rounded_by_col.get(k, [-1])[-1]
+                ):
+                    k += 1
+                k -= 1
+                if k not in self.rounded_by_col:
+                    self.rounded_by_col[k] = []
+                    bisect.insort(self.ordered_col, k)
+                self.rounded_by_col[k].append(i)
 
-    def process_west(self, new_rounded_set):
-        for rock in self.rounded:
-            res = rock - 1
-            while (
-                res.real >= 0 and res not in self.cubes and res not in new_rounded_set
-            ):
-                res -= 1
-            res += 1
-            new_rounded_set.add(res)
+    def process_west(self):
+        self.rounded_by_col = {}
+        self.ordered_col = []
+        for i in self.ordered_row:
+            for j in self.rounded_by_row[i]:
+                k = j - 1
+                while (
+                    k >= 0
+                    and (i, k) not in self.cubes
+                    and i > self.rounded_by_col.get(k, [-1])[-1]
+                ):
+                    k -= 1
+                k += 1
+                if k not in self.rounded_by_col:
+                    self.rounded_by_col[k] = []
+                    bisect.insort(self.ordered_col, k)
+                self.rounded_by_col[k].append(i)
 
 
 class Day14(Day):
@@ -138,7 +158,7 @@ class Day14(Day):
     def solve_1(reflector_str: Iterator[str]):
         grid = Grid.build_from_string(reflector_str)
         grid.process_direction(Direction.NORTH)
-        return sum(map(lambda rock: int(grid.nb_row - rock.imag), grid.rounded))
+        return grid.get_sum_from_row()
 
     @staticmethod
     def solve_2(reflector_str: Iterator[str]):
@@ -146,6 +166,7 @@ class Day14(Day):
         grid = Grid.build_from_string(reflector_str)
         r = 0
         positions = {grid.get_hash(): (r, None)}
+        totals = {}
         while r < nb_round:
             for direction in [
                 Direction.NORTH,
@@ -162,11 +183,12 @@ class Day14(Day):
                     positions[hsh] = (start, r + 1)
                     end = r
                 loop_size = end - start
-                r = nb_round - ((nb_round - r) % loop_size)
+                return totals[start + ((nb_round - start) % loop_size) - 1]
             else:
                 positions[hsh] = (r, None)
+            totals[r] = grid.get_sum()
             r += 1
-        return sum(map(lambda rock: int(grid.nb_row - rock.imag), grid.rounded))
+        return grid.get_sum()
 
     def solution_first_star(self, input_value, input_type):
         return self.solve_1(input_value)
